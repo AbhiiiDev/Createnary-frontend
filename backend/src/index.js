1ef1b88569dbf9c91106d8fc5695d33024fef0a3
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2024-06-20 16:56:00 +0530

    Backend/index.js changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,20 @@
 const express=require('express');
 const cors=require('cors');
+const calculateRoute=require('./routes/calculateRoutes');
 
 const app=express();
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 
 
-const calculateRoute=require('./routes/calculateRoutes');
-app.use('/',calculateRoute)
+app.use('/',(req,res)=>{
+    res.send('Everything in backend is working fine !')
+})
+
+
+
+app.use('/',calculateRoute);
 
 app.listen(3000,()=>{
     console.log('server is listening on port 3000');
